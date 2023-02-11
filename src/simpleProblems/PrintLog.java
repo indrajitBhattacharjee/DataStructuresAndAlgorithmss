ad2789fba7d91b4dd66142b0dddc2664fcd3596d
Indrajit Bhattacharjee <indrajit.bhattacharjeecs100@gmail.com>
2023-02-11 12:21:08 +0900

    updating the print log

--- src/simpleProblems/PrintLog.java
+++ src/simpleProblems/PrintLog.java
@@ -1,7 +1,7 @@
-
+package simpleProblems;
 public class PrintLog {
-	public static void main(String[] args) {
-    String str = "Hello World";
-		System.out.print("Print Log": + str);
+public static void main(String[] args) {
+  String str = "Hello World";
+   System.out.print("Print Log:" + str);
 	}
 }
